2a68ce690a9c3e75693e0a61aaac462d1c81ca74
Aditya-Karmalkar <adityanv.karmalkar@gmail.com>
2024-09-07 13:45:20 +0530

    upload

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+
+void main() async {
+  // * For more info on error handling, see:
+  // * https://docs.flutter.dev/testing/errors
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // turn off the # in the URLs on the web
+    usePathUrlStrategy();
+    // * Entry point of the app
+    await FlutterDisplayMode.setHighRefreshRate().catchError((e) {
+      print("Error setting the high refresh rate.");
+    });
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      systemNavigationBarColor: Colors.transparent,
+    ));
+
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    runApp(const MyApp());
+
+    // * This code will present some error UI if any uncaught exception happens
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('An error occurred'.hardcoded),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }, (Object error, StackTrace stack) {
+    // * Log any errors to console
+    debugPrint(error.toString());
+  });
+}
